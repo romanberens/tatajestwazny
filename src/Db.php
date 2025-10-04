f23ce088fa25b6157fcccea6b7cd2b713d3aef6a
romanberens <67163365+romanberens@users.noreply.github.com>
2025-10-04 10:28:46 +0200

    Improve SQLite PDO configuration

--- src/Db.php
+++ src/Db.php
@@ -5,9 +5,13 @@ final class Db {
     $this->pdo = new PDO('sqlite:' . __DIR__ . '/../db/tjw.sqlite', null, null, [
       PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
       PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+      PDO::ATTR_EMULATE_PREPARES => false,
+      PDO::ATTR_TIMEOUT => 5,
     ]);
     $this->pdo->exec("PRAGMA journal_mode=WAL;");
     $this->pdo->exec("PRAGMA foreign_keys=ON;");
     $this->pdo->exec("PRAGMA synchronous=NORMAL;");
+    $this->pdo->exec("PRAGMA busy_timeout=5000;");
+    $this->pdo->exec("PRAGMA trusted_schema=OFF;");
   }
 }
